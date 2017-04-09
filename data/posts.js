acf046ac36d62ea8a524b6a19f9e0e5285528038
Jimmy <3.1415926.candlexyz.pi+github@gmail.com>
2017-04-09 12:10:13 -0400

    added forums page, with fake posts

---
+++ data/posts.js
@@ -0,0 +1,19 @@
+post_content = {
+  "posts": [
+    {
+      "title": "Price for catsitting",
+      "author": "Alyssa",
+      "text": "Many people have been wondering how much to charge for catsitting/how much to pay catsitters. I've generally seen people charge $5/day for catsitting. That seems pretty reasonable to me. Maybe if your cat was super low-maintence or super high-maintence, you would charge a different amount, but otherwise you can't go wrong with $5."
+    },
+    {
+      "title": "Super high maintence cat",
+      "author": "Rosy",
+      "text": "Guys, if you have a super high maintence cat you should pay the catsitters extra. I was helping out a friend catsit her cat today because she was out of town for an interview, and her cat peed on the carpet three separate times! I can tell you that cleaning up cat pee is not pleasant, and I'm pretty sure I smell like cat pee now. I feel like if you cat requires a lot of extra work to catsit, you should pay at least $2/day extra."
+    },
+    {
+      "title": "Fun cat toys",
+      "author": "Bob",
+      "text": "There's lots of fun cat toys out there! My cat, in particular, likes playing with balls of yarn. I know that a lot of people have cats who like to catch live mice. If that's an issue for you, consider getting a mouse squish toy. That way, you cat can play with a toy mouse instead of a real mouse. It's more sanitary and more healthy for the cat."
+    }
+  ]
+}
