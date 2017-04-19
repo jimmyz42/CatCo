3963cebf06a9a1a0cb6889ba9d32c100d7751c58
Brenda Zanze <brendaz@mit.edu>
2017-04-19 16:06:03 -0400

    catpics and stuff

--- data/browse-cats-data.js
+++ data/browse-cats-data.js
@@ -8,42 +8,42 @@ cat_data = {
     {
       "name": "Mr. Whiskers",
       "dates": "4/17-4/19",
-      "picture": "images/ginger.jpg"
+      "picture": "images/c1.jpg"
     },
     {
-      "name": "Mr. Whiskers",
+      "name": "Claire",
+      "dates": "4/18-4/19",
+      "picture": "images/c2.jpg"
+    },
+    {
+      "name": "Mr. Cuddles",
       "dates": "4/17-4/19",
-      "picture": "images/ginger.jpg"
+      "picture": "images/c3.jpg"
     },
     {
-      "name": "Mr. Whiskers",
-      "dates": "4/17-4/19",
-      "picture": "images/ginger.jpg"
+      "name": "Angel",
+      "dates": "4/17-4/29",
+      "picture": "images/c4.jpg"
     },
     {
-      "name": "Ginger",
-      "dates": "4/7-4/9",
-      "picture": "images/ginger.jpg"
-    },
-    {
-      "name": "Ginger",
-      "dates": "4/7-4/9",
-      "picture": "images/ginger.jpg"
+      "name": "Meowies",
+      "dates": "5/7-5/9",
+      "picture": "images/c5.jpg"
     },
         {
-      "name": "Ginger",
-      "dates": "4/7-4/9",
-      "picture": "images/ginger.jpg"
+      "name": "CatCat",
+      "dates": "7/7-7/9",
+      "picture": "images/c6.jpg"
     },
     {
-      "name": "Ginger",
-      "dates": "4/7-4/9",
-      "picture": "images/ginger.jpg"
+      "name": "PigPen",
+      "dates": "8/7-8/9",
+      "picture": "images/c7.jpg"
     },
     {
-      "name": "Ginger",
-      "dates": "4/7-4/9",
-      "picture": "images/ginger.jpg"
+      "name": "Purr",
+      "dates": "8/17-8/19",
+      "picture": "images/c8.jpg"
     },
   ]
 }
